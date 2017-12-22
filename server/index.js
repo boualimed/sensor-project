6bf6ae994e3593b54453bf29cc0abf70bfea421b
unknown <bmbouali@gmail.com>
2017-12-22 13:13:39 +0100

    Made some changes

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.get('/temperature', function(req,res){
+	res.send('24°C'); 
+});
+
+
+app.get('/humidity', function(req,res){
+	res.send('48%');
+});
+
+app.listen(3000, function(){
+
+	console.log("Server Listening On Port 3000"); 
+});
+

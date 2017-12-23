8a55666174461424338daa3df7b40b5a8ef7cc27
boualimed <bmbouali@gmail.com>
2017-12-23 13:41:41 +0000

    update index sen

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
-app.get('/temperature', function(req,res){
-	res.send('24°C'); 
+const sensor = require('node-dht-sensor');
+
+app.get('/temperature', function(req,res)
+{
+	sensor.read(11, 4, function(err, temperature, humidity)
+{
+	if(!err)
+{
+	 res.send(temperature.toFixed(1) + '°C' );
+
+	}
+	
+});
+ 
 });
 
 
-app.get('/humidity', function(req,res){
-	res.send('48%');
+app.get('/humidity', function(req,res)
+{
+	sensor.read(11, 4, function(err, temperature, humidity)
+{
+	if(!err)
+{
+	res.send(humidity.toFixed(1) + '%');
+}
+});
 });
 
 app.listen(3000, function(){
